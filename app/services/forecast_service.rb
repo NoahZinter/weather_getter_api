63a9a71fa1388fc55da4e85669acb34ef1f8169e
NoahZinter <noah.zinter@gmail.com>
2021-08-07 11:55:34 -0600

    remove coverage

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,5 +1,8 @@
 class ForecastService
-  def self.get_forecast(lat, long)
-
+  def self.get_forecast(lat, lon)
+    url = 'https://api.openweathermap.org/data/2.5/onecall'
+    res = Faraday.get(url, {lat: lat, lon: lon, appid: ENV['WEATHER_API_KEY']})
+    a = JSON.parse(res.body, symbolize_names: true)
+    binding.pry
   end
 end
---
+++ spec/services/forecast_service_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ForecastService do
+  describe '.get_forecast' do
+    it 'retrieves forecast with lat and lng' do
+      lat = 39.738453
+      lng = -104.984853
+      data = ForecastService.get_forecast(lat, lng)
+    end
+  end
+end
