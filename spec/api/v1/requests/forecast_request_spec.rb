4dba53f6524b45827313210debd83d7fb4cbcc2f
NoahZinter <noah.zinter@gmail.com>
2021-08-07 10:58:59 -0600

    Route to forecast controller

---
+++ spec/api/v1/requests/forecast_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Requests' do
+  describe '#index' do
+    it 'returns a forecast' do
+      get '/api/v1/forecast?location=denver,co'
+
+      expect(response.status).to eq 200
+
+      # forecast = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
