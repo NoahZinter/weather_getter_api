f4654b7eb4a93b6ac3b91822cd15377e8858633f
NoahZinter <noah.zinter@gmail.com>
2021-08-07 18:34:23 -0600

    Test Weather poro

---
+++ app/poros/forecast.rb
@@ -0,0 +1,37 @@
+class Forecast
+  attr_reader :current,
+              :daily,
+              :hourly
+  def initialize(data)
+    @current = { datetime: time_converter(data[:current][:dt]),
+                 sunrise: time_converter(data[:current][:sunrise]),
+                 sunset: time_converter(data[:current][:sunset]),
+                 temperature: data[:current][:temp],
+                 feels_like: data[:current][:feels_like],
+                 humidity: data[:current][:humidity],
+                 uvi: data[:current][:uvi],
+                 visibility: data[:current][:visibility],
+                 conditions: data[:current][:weather][0][:description],
+                 icon: data[:current][:weather][0][:icon]
+               }
+    @daily = daily_forecaster(data[:daily])
+    @hourly = {}
+  end
+
+  def time_converter(utc_time)
+    DateTime.strptime(utc_time.to_s, '%s')
+  end
+
+  def daily_forecaster(days)
+    days.map do |h|
+      { date: time_converter(h[:dt]).to_date,
+        sunrise: time_converter(h[:sunrise]),
+        sunset: time_converter(h[:sunset]),
+        max_temp: h[:temp][:max],
+        min_temp: h[:temp][:min],
+        conditions: h[:weather][0][:description],
+        icon: h[:weather][0][:icon]
+      }
+    end
+  end
+end
---
+++ spec/poros/forecast_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe Forecast do
+  before :all do
+    @lat = 39.738453
+    @lon = -104.984853
+    @data = ForecastService.get_forecast(@lat, @lon)
+    @forecast = Forecast.new(@data)
+  end
+  describe 'initialize' do
+    it 'exists' do
+      expect(@forecast).is_a? Forecast
+    end
+
+    it 'has current hash' do
+      current = @forecast.current
+      expect(current).is_a? Hash
+      expect(current[:datetime]).is_a? DateTime
+      expect(current[:sunrise]).is_a? DateTime
+      expect(current[:sunset]).is_a? DateTime
+      expect(current[:temperature]).to be_a(Float).or be_a(Integer)
+      expect(current[:feels_like]).to be_a(Float).or be_a(Integer)
+      expect(current[:humidity]).to be_a(Float).or be_a(Integer)
+      expect(current[:visibility]).to be_a(Float).or be_a(Integer)
+      expect(current[:conditions]).is_a? String
+      expect(current[:icon]).is_a? String
+    end
+
+    it 'has daily array' do
+      daily = @forecast.daily
+      expect(daily).is_a? Array
+      daily.each do |day|
+        expect(day).is_a? Hash
+        expect(day).to have_key(:date)
+        expect(day).to have_key(:sunrise)
+        expect(day).to have_key(:sunset)
+        expect(day).to have_key(:max_temp)
+        expect(day).to have_key(:min_temp)
+        expect(day).to have_key(:conditions)
+        expect(day).to have_key(:icon)
+        expect(day[:date]).is_a? Date
+        expect(day[:sunrise]).is_a? DateTime
+        expect(day[:sunset]).is_a? DateTime
+        expect(day[:max_temp]).to be_a(Float).or be_a(Integer)
+        expect(day[:min_temp]).to be_a(Float).or be_a(Integer)
+        expect(day[:conditions]).is_a? String
+        expect(day[:icon]).is_a? String
+      end
+    end
+
+    it 'has hourly array'
+  end
+
+  describe '#time_converter' do
+    it 'converts utc time'
+
+    it 'converts past times'
+
+    it 'converts future times'
+
+    it 'does not convert negative times'
+  end
+
+  describe '#daily forecaster' do
+    it 'makes an array of hashes'
+  end
+
+  describe '#hourly_forecaster' do
+    it 'makes an array of hashes'
+  end
+end
