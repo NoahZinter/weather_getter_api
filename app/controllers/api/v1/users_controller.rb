0dc337294f9447b140b2f49ce3174526cd3f51af
NoahZinter <noah.zinter@gmail.com>
2021-08-09 15:51:58 -0600

    Create User model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    
+    if params[:password] != params[:password_confimation]
+      render json: "Error", status: 400
+    else
+      new_user = User.new(email: params[:email], password_digest: params[:password], api_key: SecureRandom.urlsafe_base64)
+      if new_user.save
+        render json: UserSerializer.new(new_user).serializable_hash.to_json
+      else
+        render json: 'Error', status: 400
+      end
+    end
   end
 end
--- spec/api/v1/requests/users_request_spec.rb
+++ spec/api/v1/requests/users_request_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Users Requests' do
   describe '#create' do
     it 'creates a user' do
-      
+      headers = {"CONTENT_TYPE"=> "application/json"}
+      user_params = {
+        "email": "e@mail.com",
+        "password": "example",
+        "password_confimation": "example"
+      }
+      post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
+
+      expect(response.status).to eq 201
     end
   end
 end
