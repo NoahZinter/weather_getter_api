b56accf0004dfffd5c1d5f0fbc5e3f670d98b746
NoahZinter <noah.zinter@gmail.com>
2021-08-09 16:14:15 -0600

    Test user creation and request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,20 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    if params[:password] != params[:password_confimation]
+    if user_params[:password] != user_params[:password_confimation]
       render json: "Error", status: 400
     else
-      new_user = User.new(email: params[:email], password_digest: params[:password], api_key: SecureRandom.urlsafe_base64)
+      new_user = User.new(email: user_params[:email], password_digest: user_params[:password], api_key: SecureRandom.urlsafe_base64)
       if new_user.save
-        render json: UserSerializer.new(new_user).serializable_hash.to_json
+        render json: UsersSerializer.new(new_user).serializable_hash.to_json, status: 201
       else
         render json: 'Error', status: 400
       end
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confimation)
   end
 end
--- spec/api/v1/requests/users_request_spec.rb
+++ spec/api/v1/requests/users_request_spec.rb
@@ -12,6 +12,20 @@ RSpec.describe 'Users Requests' do
       post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
 
       expect(response.status).to eq 201
+      user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user).to have_key(:data)
+      expect(user[:data]).is_a? Hash
+      expect(user[:data]).to have_key(:id)
+      expect(user[:data][:id]).is_a? String
+      expect(user[:data]).to have_key(:type)
+      expect(user[:data][:type]).to eq 'users'
+      expect(user[:data]).to have_key(:attributes)
+      expect(user[:data][:attributes]).is_a? Hash
+      expect(user[:data][:attributes][:email]).to eq 'e@mail.com'
+      expect(user[:data][:attributes]).not_to have_key(:password)
+      expect(user[:data][:attributes]).to have_key(:api_key)
+      expect(user[:data][:attributes][:api_key]).is_a? String
     end
   end
 end
