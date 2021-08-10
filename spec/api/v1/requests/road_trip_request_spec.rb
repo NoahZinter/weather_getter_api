0fc51a8c6cdf683727ba43850d4fa2e174508419
NoahZinter <noah.zinter@gmail.com>
2021-08-10 11:56:23 -0600

    Set up spec files

---
+++ spec/api/v1/requests/road_trip_request_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Road Trip Requests' do
+  describe 'create' do
+    it 'creates a road trip'
+  end
+end
---
+++ spec/services/directions_service_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe DirectionsService do
+  describe '.get_directions' do
+    it 'gets directions from two endpoints'
+  end
+end
